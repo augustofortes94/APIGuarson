9e1d527150c682b046cfe3d5628599869818cb9e
Augusto <augustofortes94@gmail.com>
2022-04-09 19:33:17 -0300

    model of weapon and connect to DB

--- ProyectoAPI/api/models.py
+++ ProyectoAPI/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Weapon(models.Model):
+    name=models.CharField(max_length=50)
+    muzzle=models.CharField(max_length=100, null=True, blank=True, default=None)
+    barrel=models.CharField(max_length=100, null=True, blank=True, default=None)
+    laser=models.CharField(max_length=100, null=True, blank=True, default=None)
+    optic=models.CharField(max_length=100, null=True, blank=True, default=None)
+    stock=models.CharField(max_length=100, null=True, blank=True, default=None)
+    underbarrel=models.CharField(max_length=100, null=True, blank=True, default=None)
+    magazine=models.CharField(max_length=100, null=True, blank=True, default=None)
+    ammunition=models.CharField(max_length=100, null=True, blank=True, default=None)
+    reargrip=models.CharField(max_length=100, null=True, blank=True, default=None)
+    perk=models.CharField(max_length=100, null=True, blank=True, default=None)
+    perk2=models.CharField(max_length=100, null=True, blank=True, default=None)
+    alternative=models.CharField(max_length=512, null=True, blank=True, default=None)
