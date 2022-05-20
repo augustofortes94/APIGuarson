f8b1c37abc6f6ee7ea0b4ed96f0a7a1120652033
Fortes <fortesa@spa1.gba.gov.ar>
2022-05-20 09:37:03 -0300

    model user to Use

--- APIGuarson/api/models.py
+++ APIGuarson/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
+class User(models.Model):
     firstname=models.CharField(max_length=50)
     lastname=models.CharField(max_length=50)
     username=models.CharField(max_length=50)
